a0d0727a18dc89554755e2fbb5b457b389eefeb2
ralph <wealthralph@gmail.com>
2024-10-31 04:10:23 +0100

    created the validateIndex and delete controller

---
+++ todo.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+
+}
+
+func (todos *Todos) validateIndex(index int) error {
+
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid Index")
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+
+}
+
+func (todos *Todos) delete(index int) error {
+
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = append(t[:index], t[index+1:]...)
+
+	return nil
+
+}
