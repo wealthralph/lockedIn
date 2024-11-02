8ba8476743d223e88fc5703986dead49830f3fb2
ralph <wealthralph@gmail.com>
2024-11-03 00:45:53 +0100

    Added the storage for my todos

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.add("Pray for grace")
 	todos.add("Ask the holy spirit for guidiance")
 	todos.toggle(0)
